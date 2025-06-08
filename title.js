810677af8e82f00f5154ac5b36f3303097362a13
jiarphll <0924hja@daum.net>
2025-06-08 15:02:17 +0900

    아마 최종

--- title.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#settings dd").click(function(){
-        $(".settingPlane").show();
-    });
-
-    $("#settingExit").click(function(){
-        $(".settingPlane").hide();
-    });
-});
